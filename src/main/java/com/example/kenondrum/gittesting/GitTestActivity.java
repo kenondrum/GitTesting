601b8eacfcbfefeec72a84d62121919ecec6f1ef
Ken Billingsley <kenondrum@Kens-MacBook-Pro.local>
2017-02-03 10:10:32 -0500

    Initial commit

---
+++ src/main/java/com/example/kenondrum/gittesting/GitTestActivity.java
@@ -0,0 +1,13 @@
+package com.example.kenondrum.gittesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_test);
+    }
+}
